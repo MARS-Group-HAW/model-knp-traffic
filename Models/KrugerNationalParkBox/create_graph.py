8aac335d4a156de08d07b0ed3c77127aa762af1d
Osterholz, Daniel <daniel.osterholz@haw-hamburg.de>
2021-08-03 13:22:42 +0200

    Replace create_graph.py

--- Models/KrugerNationalParkBox/create_graph.py
+++ Models/KrugerNationalParkBox/create_graph.py
@@ -3,9 +3,7 @@ from osmnx import io
 
 mars.config(log_console=True)
 
-G = mars.graph_from_place('MP325', network_type='drive', simplify=True)
-io.save_graphml(G, 'kruger_drive_graph.graphml')
-# io.save_graph_shapefile(G,"kruger_drive_graph.shp")
-# io.save_graph_geopackage(G, "kruger_drive_graph.gpkg")
+G = mars.graph_from_place('Kruger National Park', which_result=2, network_type='drive', simplify=True)
 
-
+#io.save_graph_shapefile(G,"kruger_drive_graph.shp")
+io.save_graph_geopackage(G, "kruger_drive_graph.gpkg")
