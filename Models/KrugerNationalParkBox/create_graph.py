dc4eec9eb835556bc9d63a9b4192793537a58f36
Osterholz, Daniel <daniel.osterholz@haw-hamburg.de>
2021-08-03 13:03:42 +0200

    Replace create_graph.py

--- Models/KrugerNationalParkBox/create_graph.py
+++ Models/KrugerNationalParkBox/create_graph.py
@@ -1,12 +1,11 @@
 import osmnx as mars
 from osmnx import io
 
-G = mars.graph_from_place('Kruger National Park', which_result=2, network_type='drive', simplify=True)
-#io.save_graph_shapefile(G,"kruger_drive_graph.shp")
-io.save_graph_geopackage(G, "kruger_drive_graph.gpkg")
+mars.config(log_console=True)
 
-#G2 = mars.project_graph(mars.graph_from_place('Harburg, Hamburg, Germany', network_type='drive'))
-#G3 = mars.consolidate_intersections(G2, tolerance=10, rebuild_graph=True, dead_ends=True)
+G = mars.graph_from_place('MP325', network_type='drive', simplify=True)
+io.save_graphml(G, 'kruger_drive_graph.graphml')
+# io.save_graph_shapefile(G,"kruger_drive_graph.shp")
+# io.save_graph_geopackage(G, "kruger_drive_graph.gpkg")
 
-#F = mars.pois_from_place('Harburg, Hamburg, Germany', tags={'amenity': ['parking_space']})
-#F.to_file("Parking_Harburg", driver="ESRI Shapefile")
+
