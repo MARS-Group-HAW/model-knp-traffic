25721abfb921c1a0f397504ea2b561c040cc94cb
danielOsterholz <43994199+danielOsterholz@users.noreply.github.com>
2021-09-03 12:43:07 +0200

    Add Interfaces for EventHandling

---
+++ Models/KrugerNationalPark/Misc/Events/IEventComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using Mars.Components.Services.Events;
+using Mars.Interfaces.Agents;
+
+namespace KrugerNationalPark.Misc.Events
+{
+    public interface IEventComponent
+    {
+        public void HandleEvent(MarsEvent marsEvent);
+    }
+}
