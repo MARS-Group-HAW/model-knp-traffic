de5543c69ac7ece3d007486740da56d0a3fcd30d
danielOsterholz <43994199+danielOsterholz@users.noreply.github.com>
2021-09-03 12:44:23 +0200

    Update Program

--- Models/KrugerNationalPark/Misc/Events/KnpEvent.cs
+++ Models/KrugerNationalPark/Misc/Events/KnpEvent.cs
@@ -6,22 +6,20 @@ namespace KrugerNationalPark.Misc.Events
 {
     public class KnpEvent : MarsEvent
     {
-        public readonly double Lat;
-        public readonly double Lon;
+        public readonly Position Position;
         public readonly double Duration = 1800.0;
         public readonly DateTime StartTime;
         public readonly DateTime EndTime;
 
-        public KnpEvent(double lat, double lon, DateTime startTime)
+        public KnpEvent(double lat, double lon, DateTime startTime) : this(Position.CreatePosition(lon, lat), startTime)
         {
-            Lat = lat;
-            Lon = lon;
-            StartTime = startTime;
-            EndTime = startTime.AddSeconds(Duration);
         }
 
-        public KnpEvent(Position pos, DateTime startTime) : this(pos.Latitude, pos.Longitude, startTime)
+        public KnpEvent(Position pos, DateTime startTime)
         {
+            Position = pos;
+            StartTime = startTime;
+            EndTime = startTime.AddSeconds(Duration);
         }
     }
 }
