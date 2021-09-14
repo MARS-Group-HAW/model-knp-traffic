235a30a3bc0d7b8a2df5e80ecdd6a9e68501ed90
Daniel Glake <daniel.glake@uni-hamburg.de>
2021-09-14 14:27:18 +0200

    Update van Emde Boas tree and remove XFastTrie.cs and YFastTrie.cs.

--- Models/KrugerNationalPark/Agents/Commuter.cs
+++ Models/KrugerNationalPark/Agents/Commuter.cs
@@ -165,7 +165,7 @@ namespace KrugerNationalPark.Agents
 
         public CarSteeringHandle VehicleHandle { get; set; }
 
-        public bool OvertakingActivated { get; }
+        public bool OvertakingActivated { get; set; }
         public Car Car { get; set; }
         public bool CurrentlyCarDriving => true;
 
