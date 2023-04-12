8a1a752d82cfe7c4f3cf9a3eb40875d6e9025cc9
Nima <nima.ahmady-moghaddam@haw-hamburg.de>
2023-04-12 13:48:24 +0200

    refactor: Split PoiType and PoiAccess structs into separate files

---
+++ Models/KrugerNationalPark/Misc/PoiAccess.cs
@@ -0,0 +1,8 @@
+namespace KrugerNationalPark.Misc;
+
+public struct PoiAccess
+{
+    public const string Private = "Private";
+    public const string Public = "Public";
+    public const string Staff = "Staff";
+}
