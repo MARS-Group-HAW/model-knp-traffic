e6cc9cdec049dc132113113c46d0eb020228e98f
Nima Ahmady <nima.ahmady-moghaddam@haw-hamburg.de>
2023-05-24 11:09:16 +0200

    feat: Add road surface struct
    Enable reference to road surface types in source code via variables instead of hard-coded strings

---
+++ Models/KrugerNationalPark/Misc/RoadSurface.cs
@@ -0,0 +1,11 @@
+namespace KrugerNationalPark.Misc;
+
+public struct RoadSurface
+{
+    public const string Graded = "Graded";
+    public const string Gravel = "Gravel";
+    public const string Disused = "Disused";
+    public const string Rehab = "Rehab";
+    public const string Tar = "Tar";
+    public const string TwoTrack = "Two Track";
+}
